eaf24fe8d9fde6425329488876eef94dfc094b0a
M1ck4 <m.neivandt.mn@gmail.com>
2024-11-11 05:42:20 +1000

    Update __init__.py

--- metaforge/__init__.py
+++ metaforge/__init__.py
@@ -1,4 +1,4 @@
 from .metadata_manager import Metaforge
 from .schemas import ImageMetadataSchema, ImageProcessingMetadataSchema, DatasetMetadataSchema
 
-__all__ = ['Metaforge', 'ImageMetadataSchema', 'ImageProcessingMetadataSchema', 'DatasetMetadataSchema']
+__all__ = ['Metaforge', 'ImageMetadataSchema', 'ImageProcessingMetadataSchema', 'DatasetMetadataSchema', 'FilmFrameProcessor']
